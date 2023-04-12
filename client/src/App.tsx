3872881208089391b5fcc832780af900c4affcd0
Dominika Sobiecka <dominika.sobiecka@gmail.com>
2023-04-12 18:31:22 +0200

    configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+
+import {useTranslation} from 'react-i18next';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const {t, i18n} = useTranslation();
+
+    useEffect(() => {
+        const lng = navigator.language;
+        i18n.changeLanguage(lng);
+    }, [])
+
+    const lng = navigator.language;
+
+    const handleLanguageChange = (language: string) => {
+        i18n.changeLanguage(language);
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button onClick={() => handleLanguageChange("en")}>English</button>
+                <button onClick={() => handleLanguageChange("es")}>Español</button>
+                <button onClick={() => handleLanguageChange("pl")}>Polski</button>
+                <h2>
+                    {t('translations:hello_world')}
+                </h2>
+                <span> Browser Language: {lng} </span>
+            </header>
+        </div>
+    );
 }
 
 export default App;
